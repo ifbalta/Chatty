ca6715d325c1a2fe8156883ce3241199886f891a
Isabel Francesca Baltazar <cheska.baltazar@gmail.com>
2016-03-17 21:12:57 +1300

    Finally got angular controller to work!! YES!!

--- controllers/chatcontroller.js
+++ controllers/chatcontroller.js
@@ -1,14 +1,22 @@
-// controls ChatForm
+/* 
+  Controls ChatForm
+
+  -- Setting up an Angular Form
+  ---- Declare ng-app
+  ---- Declare ng-controller
+  ---- Go nuts.
+
+*/
 console.log("found ChatController");
 
 var chatApp = angular.module('chatApp', []);
-
 chatApp.controller ('ChatController', ['$scope', '$http', function ($scope, $http) {
   console.log("Here's the mail, it never fails!");
 
   $scope.chat = [
     'message'
   ];
+
 
   $scope.master = {};
 
@@ -17,3 +25,17 @@ chatApp.controller ('ChatController', ['$scope', '$http', function ($scope, $htt
     $scope.master = angular.copy(chat);
   }
 }]);
+
+console.log('Finished with controller');
+
+/*
+    console.log('hey')
+    var app = angular.module('myApp', []);
+    app.controller('myCtrl', function($scope) {
+        $scope.firstName = "John";
+        $scope.lastName = "Doe";
+    });
+
+
+
+*/
